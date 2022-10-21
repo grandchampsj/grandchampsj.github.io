2a192e0d3237072a9e5ef353c91680bd8ca0c70e
Jennifer Grandchamps <101316127+grandchampsj@users.noreply.github.com>
2022-10-20 23:15:50 -0400

    Update script.js
    added nav eventListener

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const open = document.getElementById("open")
+const close = document.getElementById("close")
+const mainContainer = document.querySelector(".mainContainer")
+
+open.addEventListener("click", () => mainContainer.classList.add("show-nav"))
+
+close.addEventListener("click", () => mainContainer.classList.remove("show-nav"))
+
+
+
 const panels = document.querySelectorAll(".panel") //querySelector allows to select ANY elements
 
 //forEach array calls to add the eventListener to element "panel" and listener will add panel to class list active

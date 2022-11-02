30aeb404e1bb94c79fbd7e54ff60324a9ff403ac
Jennifer Grandchamps <101316127+grandchampsj@users.noreply.github.com>
2022-11-02 09:57:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//navbar
+
 const open = document.getElementById("open")
 const close = document.getElementById("close")
 const mainContainer = document.querySelector(".mainContainer")
@@ -7,7 +9,7 @@ open.addEventListener("click", () => mainContainer.classList.add("show-nav"))
 close.addEventListener("click", () => mainContainer.classList.remove("show-nav"))
 
 
-
+// project panel
 const panels = document.querySelectorAll(".panel") //querySelector allows to select ANY elements
 
 //forEach array calls to add the eventListener to element "panel" and listener will add panel to class list active
@@ -23,5 +25,31 @@ function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove("active")
     })
-};
+}
 
+//page transition
+
+window.onload = () => {
+    const transition_el = document.querySelector(".transition");
+    const anchors = document.querySelectorAll("a");
+
+    setTimeout(() => {
+        transition_el.classList.remove("is-active");
+    }, 500);
+
+    for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+
+        anchor.addEventListener("click", e => {
+            e.preventDefault();
+            let target = e.target.href;
+
+            transition_el.classList.add("is-active");
+
+            setTimeout(() => {
+                window.location.href = target;
+            }, 500);
+
+        })
+    }
+}
